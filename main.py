a7671f02f0d0a6aa695f6c750dcdf4352cea268a
Roi <thedarkhermit@gmail.com>
2021-04-28 19:56:12 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,34 @@
+import numpy as np
+
+
+def gradient_method(f, gf, lsearch, x0, epsilon=1e-5):
+    x = x0
+    fs = []
+    gs = []
+    gval = gf(x)
+    iter = 0
+    while np.linalg.norm(gval) >= epsilon:
+        iter += 1
+        t = lsearch(f, x, gf(x))
+        x = x - t * gval
+        print('iter= {:2d} f(x)={:10.10f}'.format(iter, f(x)))
+        gval = gf(x)
+        fs.append(f(x))
+        gs.append(np.linalg.norm(gval))
+
+    return x
+
+
+def lsearch(f, x, gf):
+    return 0.1
+
+
+def main():
+    f = lambda x: x ** 2
+    gf = lambda x: 2 * x
+    x0 = 50
+    gradient_method(f, gf, lsearch, x0)
+
+
+if __name__ == "__main__":
+    main()
